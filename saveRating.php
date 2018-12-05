f38b8b81faa99449e08cf2047b246726dbde5245
helix <garychang9@gmail.com>
2018-12-04 23:01:40 -0800

    Corrected review date

--- saveRating.php
+++ saveRating.php
@@ -5,6 +5,8 @@ if(!empty($_POST['rating']) && !empty($_POST['itemId'])){
 	$itemId = $_POST['itemId'];
 	$userID = $_SESSION["id"];	
 	$comment = htmlspecialchars($_POST["comment"]);
+	date_default_timezone_set('UTC');
+	date_default_timezone_set('America/Los_Angeles');
 	$insertRating = "INSERT INTO restaurant_rating (itemId, userId, ratingNumber, username, comments, created, modified) VALUES ('".$itemId."', '".$userID."', '".$_POST['rating']."', '".$_SESSION['username']."', '".$comment."', '".date("Y-m-d H:i:s")."', '".date("Y-m-d H:i:s")."')";
 	mysqli_query($link, $insertRating) or die("database error: ". mysqli_error($link));
 	$updateScore = "UPDATE markers SET reviews = reviews + 1, score = score + '".$_POST['rating']."' WHERE id = '".$itemId."' ";
